2329b6c83710c79e89b74b8743622f1d804e5a8a
atharva <atharvak.8023@gmail.com>
2024-03-10 01:52:05 +0530

    chess board

---
+++ chessEngine.py
@@ -0,0 +1,18 @@
+class GameState():
+    def __init__(self):
+        # 8x8 2d list, each element has 2 characters
+        # The first character represents the color of the piece
+        # The second character represents the type of the piece
+        # "--" represents an empty space with no piece
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
